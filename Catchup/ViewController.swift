b165819b1d28c80d647987003714d863aa90cf80
Xinxing Jiang <jiangxinxing09@gmail.com>
2016-03-30 22:46:03 +0800

    feat: backbone of two sorting handler
    Implement alert controller for two sorting buttons, no actual handling yet.

--- Catchup/ViewController.swift
+++ Catchup/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var alertController: UIAlertController!
+    
     // MARK: - VC life cycle
     
     override func viewDidLoad() {
@@ -20,13 +22,27 @@ class ViewController: UIViewController {
     // MARK: - Click sort by name button
     
     @IBAction func sortByName() {
-        
+        alertController = UIAlertController()
+        alertController.addAction(UIAlertAction(title: "A to Z", style: UIAlertActionStyle.Default, handler: { _ in
+            print("A to Z")
+        }))
+        alertController.addAction(UIAlertAction(title: "Z to A", style: UIAlertActionStyle.Default, handler: { _ in
+            print("Z to A")
+        }))
+        presentViewController(alertController, animated: true, completion: nil)
     }
 
     // MARK: - Click sort by date button
     
     @IBAction func sortByDate() {
-        
+        alertController = UIAlertController()
+        alertController.addAction(UIAlertAction(title: "Most recent", style: UIAlertActionStyle.Default, handler: { _ in
+            print("most recent")
+        }))
+        alertController.addAction(UIAlertAction(title: "Least recent", style: UIAlertActionStyle.Default, handler: { _ in
+            print("least recent")
+        }))
+        presentViewController(alertController, animated: true, completion: nil)
     }
     
     // MARK: - Click add button
