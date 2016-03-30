298f686391331a9d64ee81a5e46a97417d974a78
Xinxing Jiang <jiangxinxing09@gmail.com>
2016-03-30 22:11:37 +0800

    feat: Setup. UI. Button click events.
    1. Setup project; 2. basic UI backbone; 3. three button click event handlers.

---
+++ Catchup/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  Catchup
+//
+//  Created by Xinxing Jiang on 3/30/16.
+//  Copyright © 2016 PalmTech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // MARK: - VC life cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    // MARK: - Click sort by name button
+    
+    @IBAction func sortByName() {
+        
+    }
+
+    // MARK: - Click sort by date button
+    
+    @IBAction func sortByDate() {
+        
+    }
+    
+    // MARK: - Click add button
+    
+    @IBAction func add() {
+        
+    }
+}
+
