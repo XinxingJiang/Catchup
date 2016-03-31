ea4747abcfebd52b1dc67533ea8bc4598d6d2445
Xinxing Jiang <jiangxinxing09@gmail.com>
2016-03-31 09:02:08 +0800

    feat: Add item: submit button clickable condition, and insert/remove date text field seperator autom
    1. make submit button clickable iff. name text field is nonempty and date text field is valid (not
    checking yet); 2. auto insert/delete date seperator for date text field.

--- Catchup/ViewController.swift
+++ Catchup/ViewController.swift
@@ -8,11 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var tableView: UITableView!
     
     var alertController: UIAlertController!
+    var submitAction: UIAlertAction!
+    var nameTextField: UITextField!
+    var dateTextField: UITextField!
 
     var items: [ItemModel]!
     
@@ -53,18 +56,73 @@ class ViewController: UIViewController {
     
     @IBAction func add() {
         alertController = UIAlertController(title: "New item", message: "", preferredStyle: UIAlertControllerStyle.Alert)
+        
         alertController.addTextFieldWithConfigurationHandler { textField in
+            self.nameTextField = textField
             textField.placeholder = "Name"
+            textField.delegate = self
         }
+        
         alertController.addTextFieldWithConfigurationHandler { textField in
+            self.dateTextField = textField
             textField.placeholder = "MM/DD/YYYY"
             textField.keyboardType = .NumberPad
+            textField.delegate = self
         }
-        alertController.addAction(UIAlertAction(title: "Submit", style: UIAlertActionStyle.Default, handler: { _ in
-            print("submit")
-        }))
+        
+        submitAction = UIAlertAction(title: "Submit", style: .Default) { (_) in
+            self.submit()
+        }
+        submitAction.enabled = false
+        alertController.addAction(submitAction)
+
         alertController.addAction(UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Cancel, handler: nil))
         presentViewController(alertController, animated: true, completion: nil)
+    }
+    
+    // MARK: - Alert controller delegate
+    
+    // MARK: - Text field delegate
+    
+    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+        let stringAfterEdit = NSString(string: textField.text!).stringByReplacingCharactersInRange(range, withString: string)
+        if textField == nameTextField {
+            submitAction.enabled = !stringAfterEdit.isEmpty && isDateValid(date: dateTextField.text!)
+        }
+        if textField == dateTextField {
+            submitAction.enabled = !nameTextField.text!.isEmpty && isDateValid(date: stringAfterEdit)
+            
+            // lazily insert or delete seperator /
+            let stringLengthBeforeEdit = textField.text!.characters.count
+            let stringLengthAfterEdit = stringAfterEdit.characters.count
+            
+            // insert seperator
+            if (stringLengthAfterEdit == 3 || stringLengthAfterEdit == 6) && stringLengthAfterEdit > stringLengthBeforeEdit {
+                textField.text = textField.text! + "/" + string
+                return false
+            }
+            
+            // delete seperator
+            if (stringLengthAfterEdit == 2 || stringLengthAfterEdit == 5) && stringLengthAfterEdit < stringLengthBeforeEdit {
+                textField.text = stringAfterEdit.substringToIndex(stringAfterEdit.endIndex.advancedBy(-1))
+                return false
+            }
+            
+            // length upper limit
+            if stringLengthAfterEdit > 10 {
+                return false
+            }
+        }
+        return true
+    }
+    
+    func submit() {
+        
+    }
+    
+    func isDateValid(date date: String) -> Bool {
+        // TO DO
+        return !date.isEmpty
     }
 }
 
