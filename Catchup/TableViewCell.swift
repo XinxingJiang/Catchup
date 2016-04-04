f5651389ab6e84d18f784f57174c2e74134649e4
Xinxing Jiang <jiangxinxing09@gmail.com>
2016-04-04 16:43:31 +0800

    feat: Implement table view cell.
    Implement table view cell.

--- Catchup/TableViewCell.swift
+++ Catchup/TableViewCell.swift
@@ -12,15 +12,17 @@ class TableViewCell: UITableViewCell {
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var dateLabel: UILabel!
     
+    var item: [String: String]!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        nameLabel.text = item["name"]
+        dateLabel.text = item["date"]
     }
 
     override func setSelected(selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
         // Configure the view for the selected state
     }
-
 }
